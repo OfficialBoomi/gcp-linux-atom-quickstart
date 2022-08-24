e57d0d27babb81fed18cad75d3332f70076cc1d2
Ganesh-Yeole <92855225+Ganesh-Yeole@users.noreply.github.com>
2022-08-24 10:32:02 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,27 @@
+
+output "Bastion_Host" {
+
+  value=google_compute_instance.vm1.name
+
+}
+
+output "Boomi_instance" {
+
+  value=google_compute_instance.vm2.name
+
+}
+
+output "External_IP_of_Bastion_host" {  
+    
+  value = "${google_compute_instance.vm1[*].network_interface.0.access_config.0.nat_ip}"  
+}
+
+
+output "NOTE"{  
+
+ 
+ value =  "Each deployment takes about 10 minutes to complete. It may take 5 minutes for atom to show up on the platform after the deployment is completed"
+ 
+}
+
+
